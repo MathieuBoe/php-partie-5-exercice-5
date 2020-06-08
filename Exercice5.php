ad0c0d87f1c6a21501ddafe12b1122272e1853a4
MathieuBoe <mathieuboe60@gmail.com>
2020-06-08 11:19:06 +0200

    Exercice complet

---
+++ Exercice5.php
@@ -0,0 +1,9 @@
+<?php
+$var1 = 5;
+$var2 = 'Test';
+$title = 'Exercice5';
+
+include 'header.php';
+$departement = [02 => 'Aisne', 59 => 'Nord', 60 => 'Oise', 62 => 'Pas-de-Calais', 80 => 'Somme'];
+
+include 'footer.php';?>
